bf3c5d03f46d0e2f463d3ec693b3c41860494fe3
jhonatanppsilva <jhonatannao@hotmail.com>
2015-08-04 19:21:16 -0400

    Conexao com o banco via JDBC

---
+++ src/main/java/br/com/fabricadeprogramador/persistencia/jdbc/ConexaoFactory.java
@@ -0,0 +1,24 @@
+package br.com.fabricadeprogramador.persistencia.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoFactory {
+	
+	public static Connection getConnection() {
+
+		Connection c=null;
+		try {
+			c = DriverManager .getConnection("jdbc:postgresql://localhost:5432/fabricawebdb", "postgres","postgres");
+		} catch (SQLException e) {
+			throw new RuntimeException("não conectou", e);
+			
+			
+		}
+		return c;
+	}
+
+
+
+}
